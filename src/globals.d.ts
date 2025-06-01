c4c1e419460d9b78a89f695481e8614e8a3a6a3e
xwfe <xw522@qq.com>
2025-06-01 18:46:39 +0800

    refactor: migrate code-editor to ts and scss

---
+++ src/globals.d.ts
@@ -0,0 +1,11 @@
+declare module 'electron'
+declare module 'path'
+declare module 'fs'
+declare module 'url'
+declare module 'crypto'
+declare var require: any
+declare var __dirname: string
+declare namespace Electron { interface Clipboard {} }
+declare var Buffer: any
+declare var ace: any
+interface Window { editorKey: string }
